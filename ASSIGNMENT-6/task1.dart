d80080bbc9c4e1ef8ee389d7f17b544bdb232951
Syed Ali Hashmi <160578567+alihashmi2288@users.noreply.github.com>
2024-09-28 23:33:44 +0500

    Delete ASSIGNMENT-6 directory

--- ASSIGNMENT-6/task1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  List names = ["Ali", "Bilal", "Huzaifa", "Shahmir"];
-  print(names);
-}
-
-
-
- //Q.1 Create a list of names and print all names using list.
--- ASSIGNMENT-6/task14.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  int i = 10;
-  while (i >= 0) {
-    print(i);
-    i--;
-  }
-}
-
-//Q.14:Implement Dart code to print the first 10 natural numbers in reverse order using a while loop.
